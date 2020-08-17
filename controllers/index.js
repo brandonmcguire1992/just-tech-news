5e873494a7bc67c50b4f02bb32a21a12f2429b87
Brandon McGuire <brandonmcguire1992@gmail.com>
2020-08-17 14:50:07 -0500

    add homepage handlebars

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
+const homeRoutes = require('./home-routes');
 
 // brings in api/index.js
 const apiRoutes = require('./api');
 
 // prefixes routes in api/index.js with /api
 router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
 
 router.use((req, res) => {
   res.status(404).end();
