d1b47f7165dabd9db9f51f2117ee8cff38fa7d8a
Brandon McGuire <brandonmcguire1992@gmail.com>
2020-08-18 15:16:57 -0500

    finish dashboard

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 
-const userRoutes = require('./user-routes');
-const postRoutes = require('./post-routes');
-const commentRoutes = require('./comment-routes');
+// collect endpoints
+const userRoutes = require('./user-routes.js');
+const postRoutes = require('./post-routes.js');
+const commentRoutes = require('./comment-routes.js');
 
+// prefixed names
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
 router.use('/comments', commentRoutes);
--- controllers/index.js
+++ controllers/index.js
@@ -1,15 +1,17 @@
 const router = require('express').Router();
-const homeRoutes = require('./home-routes');
 
 // brings in api/index.js
 const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes.js');
+const dashboardRoutes = require('./dashboard-routes.js');
 
 // prefixes routes in api/index.js with /api
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
+router.use('/dashboard', dashboardRoutes);
 
 router.use((req, res) => {
-  res.status(404).end();
+	res.status(404).end();
 });
 
 module.exports = router;
